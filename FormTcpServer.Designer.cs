4578ff38a20985d618514cd6df0c3a862014415a
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-09-29 19:09:27 +0900

    Update

--- FormTcpServer.Designer.cs
+++ FormTcpServer.Designer.cs
@@ -30,11 +30,20 @@
         {
             this.lblIpAddress = new System.Windows.Forms.Label();
             this.lblPort = new System.Windows.Forms.Label();
+            this.textBoxIpAddress = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.labelRcvData = new System.Windows.Forms.Label();
+            this.textBoxRcvData = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblIpAddress
             // 
-            this.lblIpAddress.Location = new System.Drawing.Point(70, 47);
+            this.lblIpAddress.Location = new System.Drawing.Point(35, 26);
             this.lblIpAddress.Name = "lblIpAddress";
             this.lblIpAddress.Size = new System.Drawing.Size(104, 23);
             this.lblIpAddress.TabIndex = 0;
@@ -43,24 +52,123 @@
             // 
             // lblPort
             // 
-            this.lblPort.Location = new System.Drawing.Point(70, 84);
+            this.lblPort.Location = new System.Drawing.Point(35, 67);
             this.lblPort.Name = "lblPort";
             this.lblPort.Size = new System.Drawing.Size(104, 23);
             this.lblPort.TabIndex = 1;
             this.lblPort.Text = "Port";
             this.lblPort.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
+            // textBoxIpAddress
+            // 
+            this.textBoxIpAddress.Location = new System.Drawing.Point(166, 28);
+            this.textBoxIpAddress.Name = "textBoxIpAddress";
+            this.textBoxIpAddress.Size = new System.Drawing.Size(235, 19);
+            this.textBoxIpAddress.TabIndex = 2;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(166, 69);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(235, 19);
+            this.textBoxPort.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.White;
+            this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(166, 108);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.OnBtnClickStart);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.White;
+            this.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStop.Location = new System.Drawing.Point(258, 108);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.OnBtnClickStop);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Location = new System.Drawing.Point(35, 148);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(104, 23);
+            this.labelStatus.TabIndex = 6;
+            this.labelStatus.Text = "Status";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(166, 150);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ReadOnly = true;
+            this.textBoxStatus.Size = new System.Drawing.Size(235, 19);
+            this.textBoxStatus.TabIndex = 7;
+            // 
+            // labelRcvData
+            // 
+            this.labelRcvData.Location = new System.Drawing.Point(35, 198);
+            this.labelRcvData.Name = "labelRcvData";
+            this.labelRcvData.Size = new System.Drawing.Size(104, 23);
+            this.labelRcvData.TabIndex = 8;
+            this.labelRcvData.Text = "Recieve Data";
+            this.labelRcvData.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxRcvData
+            // 
+            this.textBoxRcvData.Location = new System.Drawing.Point(65, 224);
+            this.textBoxRcvData.Multiline = true;
+            this.textBoxRcvData.Name = "textBoxRcvData";
+            this.textBoxRcvData.ReadOnly = true;
+            this.textBoxRcvData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxRcvData.Size = new System.Drawing.Size(619, 181);
+            this.textBoxRcvData.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.White;
+            this.btnClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnClear.ForeColor = System.Drawing.Color.Black;
+            this.btnClear.Location = new System.Drawing.Point(166, 198);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.OnBtnClickClear);
+            // 
             // FormTcpServer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(725, 426);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.textBoxRcvData);
+            this.Controls.Add(this.labelRcvData);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIpAddress);
             this.Controls.Add(this.lblPort);
             this.Controls.Add(this.lblIpAddress);
             this.Name = "FormTcpServer";
             this.Text = "TCP Server";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +176,15 @@
 
         private System.Windows.Forms.Label lblIpAddress;
         private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox textBoxIpAddress;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Label labelRcvData;
+        private System.Windows.Forms.TextBox textBoxRcvData;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- FormTcpServer.cs
+++ FormTcpServer.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,128 @@ namespace TcpServerWinFormCSharp
 {
     public partial class FormTcpServer : Form
     {
+        private Thread m_thread;
+        private TcpListener m_tcpListener;
+        private string m_strIpAddress;
+        private int m_nPort;
+        private bool m_bEnd;
+
+        public Thread Thread
+        {
+            set { m_thread = value; }
+            get { return m_thread; }
+        }
+
+        public TcpListener TcpListener
+        {
+            set { m_tcpListener = value; }
+            get { return m_tcpListener; }
+        }
+
+        public string IpAddress
+        {
+            set { m_strIpAddress = value; }
+            get { return m_strIpAddress; }
+        }
+
+        public int Port
+        {
+            set { m_nPort = value; }
+            get { return m_nPort; }
+        }
+
+        public bool End
+        {
+            set { m_bEnd = value; }
+            get { return m_bEnd; }
+        }
+
         public FormTcpServer()
         {
             InitializeComponent();
+
+            Init();
+        }
+
+        ~FormTcpServer()
+        {
+        }
+
+        public void Init()
+        {
+            m_thread = null;
+            m_tcpListener = null;
+            m_strIpAddress = "127.0.0.1";
+            m_nPort = 9999;
+            m_bEnd = false;
+
+            textBoxIpAddress.Text = m_strIpAddress;
+            textBoxPort.Text = m_nPort.ToString();
+        }
+
+        private void OnBtnClickStart(object sender, EventArgs e)
+        {
+            m_strIpAddress = textBoxIpAddress.Text.ToString();
+            m_nPort = int.Parse(textBoxPort.Text.ToString());
+
+            m_thread = new Thread(DataListener);
+            m_thread.Start();
+        }
+
+        public void DataListener()
+        {
+            try
+            {
+                m_tcpListener = new TcpListener(IPAddress.Parse(m_strIpAddress), m_nPort);
+                m_tcpListener.Start();
+
+                Invoke(new Action<string>(SetTextStatus), "Server started.");
+                TcpClient client = m_tcpListener.AcceptTcpClient();
+
+                while (client.Connected)
+                {
+                    NetworkStream networkStream = client.GetStream();
+
+                    byte[] aryData = new byte[4096];
+                    int nSize = networkStream.Read(aryData, 0, aryData.Length);
+                    string strGetText = System.Text.Encoding.Default.GetString(aryData, 0, nSize);
+                    Invoke(new Action<string>(SetTextRcvData), strGetText);
+                }
+            }
+            catch(Exception)
+            {
+                Invoke(new Action<string>(SetTextStatus), "Server terminated.");
+
+                return;
+            }
+        }
+
+        public void SetTextStatus(string _strText)
+        {
+            textBoxStatus.Text = _strText.ToString();
+
+            return;
+        }
+
+        public void SetTextRcvData(string _strText)
+        {
+            textBoxRcvData.Text = _strText.ToString() + "\n";
+
+            return;
+        }
+
+        public void OnBtnClickClear(object sender, EventArgs e)
+        {
+            textBoxRcvData.Text = "";
+
+            return;
+        }
+
+        public void OnBtnClickStop(object sender, EventArgs e)
+        {
+            m_tcpListener.Stop();
+
+            return;
         }
     }
 }
